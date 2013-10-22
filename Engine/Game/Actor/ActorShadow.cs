59a6fb6fa4c73283d1b7efc1804978718b34dcae
drawlabs <ryan@drawlabs.com>
2013-10-21 19:15:44 -0700

    Shadow update.

--- Engine/Game/Actor/ActorShadow.cs
+++ Engine/Game/Actor/ActorShadow.cs
@@ -32,7 +32,7 @@ namespace Engine.Game.Actor {
                 Vector3 bottomPoint = objectParent.transform.position - Vector3.up * 1;
                 Vector3 collisionVector = bottomPoint - topPoint;
 
-                int terrainMask = 1 << LayerMask.NameToLayer("GameGroundShadows");
+                int terrainMask = 1 << LayerMask.NameToLayer("GameGround");
                 if (Physics.Raycast(topPoint, collisionVector, out hit, 100.0f, terrainMask)) {
                     surfaceNormal = hit.normal;
 
