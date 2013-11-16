be5cae7790d9c67d67e7d739569c6f82fa64c493
drawlabs <ryan@drawlabs.com>
2013-11-16 02:07:35 -0700

    Color customization presets fixes.

--- Engine/Extensions/ColorHelper.cs
+++ Engine/Extensions/ColorHelper.cs
@@ -6,11 +6,23 @@ using UnityEngine;
 public static class ColorHelper {
 
     public static Color FromRGB(float r, float g, float b) {
-        return new Color(r/255.0f, g/255.0f, b/255.0f);
+        return new Color(r / 255.0f, g / 255.0f, b / 255.0f);
     }
 
     public static Color FromRGB(float r, float g, float b, float a) {
-        return new Color(r/255.0f, g/255.0f, b/255.0f, a/1.0f);
+        return new Color(r / 255.0f, g / 255.0f, b / 255.0f, a / 1.0f);
     }
-	
+    
+    // Note that Color32 and Color implictly convert to each other. You may pass a Color object to this method without first casting it.
+    public static string ColorToHex(Color32 color) {
+        string hex = color.r.ToString("X2") + color.g.ToString("X2") + color.b.ToString("X2");
+        return hex;
+    }
+    
+    public static Color HexToColor(string hex) {
+        byte r = byte.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
+        byte g = byte.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
+        byte b = byte.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
+        return new Color32(r, g, b, 255);
+    }
 }
--- Engine/Extensions/StringExtensions.cs
+++ Engine/Extensions/StringExtensions.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections;
+using System.Globalization;
+using System.Text;
 using UnityEngine;
 
 public static class StringExtensions {
@@ -11,5 +13,21 @@ public static class StringExtensions {
 
     public static string pathForDocumentsResource(string file) {
         return PathUtil.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), file);
+    }
+
+    public static string ToHex(this int value) {
+        return String.Format("{0:X}", value);
+    }
+
+    public static string ToHexPadded(this int value) {
+        return String.Format("0x{0:X}", value);
+    }
+
+    public static int FromHex(string value) {
+        // strip the leading 0x
+        if ( value.StartsWith("0x", StringComparison.OrdinalIgnoreCase)) {
+            value = value.Substring(2);
+        }
+        return Int32.Parse(value, NumberStyles.HexNumber);
     }
 }
