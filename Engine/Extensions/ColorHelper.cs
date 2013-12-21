0ea07b07533fe3cbbeedcc7c7b58c633613b27ed
drawlabs <ryan@drawlabs.com>
2013-12-20 19:55:41 -0700

    Data for ui colors, character colors, loading, ec.

--- Engine/Extensions/ColorHelper.cs
+++ Engine/Extensions/ColorHelper.cs
@@ -1,9 +1,71 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using Engine.Utility;
 using UnityEngine;
 
+public class ColorItem {
+    public float r = 0f;
+    public float g = 0f;
+    public float b = 0f;
+    public float a = 0f;
+
+    public ColorItem() {
+        
+    }
+    
+    public ColorItem(float r, float g, float b) {
+        Set(r, g, b, 255);
+    }
+
+    public ColorItem(float r, float g, float b, float a) {
+        Set(r, g, b, a);
+    }
+        
+    public void Set(float rTo, float gTo, float bTo, float aTo) {
+        r = rTo;
+        g = gTo;
+        b = bTo;
+        a = aTo;
+    }
+    public void Set(List<double> color) {
+        if(color == null) {
+            return;
+        }
+
+        if(color.Count > 0) 
+            r = (float)color[0];
+        
+        if(color.Count > 1) 
+            g = (float)color[1];
+        
+        if(color.Count > 2) 
+            b = (float)color[2];
+        
+        if(color.Count > 3) 
+            a = (float)color[3];
+    }
+
+    public Color GetColor(List<double> color) {
+        if(color == null) {
+            return Color.white;
+        } 
+
+        Set(color);
+
+        return GetColor();
+    }
+
+    public Color GetColor() {
+        return new Color(r / 255.0f, g / 255.0f, b / 255.0f, a / 255.0f);
+    }
+}
+
 public static class ColorHelper {
+
+    public static Color FromRGB(List<double> color) {
+        return new ColorItem().GetColor(color);
+    }
 
     public static Color FromRGB(float r, float g, float b) {
         return new Color(r / 255.0f, g / 255.0f, b / 255.0f);
--- Engine/Game/App/BaseApp/BaseGameColors.cs
+++ Engine/Game/App/BaseApp/BaseGameColors.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 
+using UnityEngine;
+
 public class BaseGameColors<T> : DataObjects<T> where T : new() {
     private static T current;
     private static volatile BaseGameColors<T> instance;
-    private static object syncRoot = new Object();
+    private static System.Object syncRoot = new System.Object();
 
     private string BASE_DATA_KEY = "game-color-data";
 
@@ -53,10 +55,85 @@ public class BaseGameColors<T> : DataObjects<T> where T : new() {
     }
 }
 
+
+public class GameColorValue : DataObject {
+
+    public virtual string type {
+        get {
+            return Get<string>(BaseDataObjectKeys.type, BaseGameColorKeys.rgba);
+        }
+        
+        set {
+            Set<string>(BaseDataObjectKeys.type, value);
+        }
+    }
+
+    public virtual List<double> rgba {
+        get {
+            return Get<List<double>>(BaseGameColorKeys.rgba);
+        }
+        
+        set {
+            Set<List<double>>(BaseGameColorKeys.rgba, value);
+        }
+    }
+    
+    public GameColorValue() {
+        Reset();
+    }
+    
+    public void SetColor(float r, float g, float b, float a) {
+
+        if(rgba == null) {            
+            rgba = new List<double>();
+            rgba.Add(r);
+            rgba.Add(g);
+            rgba.Add(b);
+            rgba.Add(a);
+        }
+        else {
+        
+            rgba[0] = r;
+            rgba[1] = g;
+            rgba[2] = b;
+            rgba[3] = a;
+        }
+    }
+
+    public Color GetColor() {
+        return ColorHelper.FromRGB(rgba);
+    }
+    
+    public void Reset() {
+        float r = 1;
+        float g = 1;
+        float b = 1;
+        float a = 1;
+        SetColor(r, g, b, a);
+        type = BaseGameColorKeys.rgba;
+    }
+}
+
+public class BaseGameColorKeys {
+    public static string color = "color";
+    public static string rgba = "rgba";
+    public static string type = "type";
+}
+
 public class BaseGameColor : GameDataObject {
 
     // Attributes that are added or changed after launch should be like this to prevent
     // profile conversions.
+
+    public virtual GameColorValue color {
+        get {
+            return Get<GameColorValue>(BaseGameColorKeys.color);
+        }
+        
+        set {
+            Set<GameColorValue>(BaseGameColorKeys.color, value);
+        }
+    }
 
     public BaseGameColor() {
         Reset();
@@ -68,6 +145,10 @@ public class BaseGameColor : GameDataObject {
 
     public void Clone(BaseGameColor toCopy) {
         base.Clone(toCopy);
+    }     
+    
+    public Color GetColor() {
+        return color.GetColor();
     }
 
     // Attributes that are added or changed after launch should be like this to prevent
