fd8ba7a027e4956de0d7e214762fd07a026e303e
drawlabs <ryan@drawlabs.com>
2014-04-22 10:28:03 -0700

    Update shadows.  Update results screen.  Update throttling speed modifier within safe range.

--- Engine/Vehicle/AI/Scripts/TriggerSound.cs
+++ Engine/Vehicle/AI/Scripts/TriggerSound.cs
@@ -1,38 +1,31 @@
 using UnityEngine;
 using System.Collections;
 
-public class TriggerSound : MonoBehaviour
-{
-    public string tagName1="";
+public class TriggerSound : MonoBehaviour {
+    public string tagName1 = "";
     public string tagName2 = "";
     public AudioClip triggerSound;
     public float soundVolume = 1.0f;
-
     private AudioSource triggerAudioSource;
 
-    void Awake()
-    {
+    void Awake() {
         InitSound(out triggerAudioSource, triggerSound, soundVolume, false);
     }
-
-	
-    void InitSound(out AudioSource myAudioSource, AudioClip myClip, float myVolume, bool looping)
-    {
-        myAudioSource = gameObject.AddComponent("AudioSource") as AudioSource;
-        myAudioSource.playOnAwake = false;
-        myAudioSource.clip = myClip;
-        myAudioSource.loop = looping;
-        myAudioSource.volume = myVolume;
+    
+    void InitSound(out AudioSource audioSource, AudioClip clip, float volume, bool looping) {
+        audioSource = gameObject.AddComponent("AudioSource") as AudioSource;
+        audioSource.playOnAwake = false;
+        audioSource.clip = clip;
+        audioSource.loop = looping;
+        audioSource.volume = (float)GameProfiles.Current.GetAudioEffectsVolume();
         //myAudioSource.rolloffMode = AudioRolloffMode.Linear;
     }
 
-    void OnTriggerEnter(Collider other) 
-    {
+    void OnTriggerEnter(Collider other) {
         //if (other.gameObject.tag == tagName1 || other.gameObject.tag == tagName2) //2013-08-02
-        if (other.gameObject.CompareTag(tagName1) || other.gameObject.CompareTag(tagName2)) //2013-08-02
-        {
-			if (other.gameObject.layer != 2) //2011-12-27
-            	triggerAudioSource.Play();
+        if (other.gameObject.CompareTag(tagName1) || other.gameObject.CompareTag(tagName2)) { //2013-08-02
+            if (other.gameObject.layer != 2) //2011-12-27
+                triggerAudioSource.Play();
         }
 
     }
