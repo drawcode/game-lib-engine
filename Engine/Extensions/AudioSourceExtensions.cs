adbbde9227db254ae6caa29d5b643b7c9897f6d0
drawlabs <ryan@drawlabs.com>
2014-06-17 01:54:15 -0700

    Update audio fade between tracks and ui/game audio.

--- Engine/Extensions/AudioSourceExtensions.cs
+++ Engine/Extensions/AudioSourceExtensions.cs
@@ -16,6 +16,10 @@ public static class AudioSourceExtensions {
         if(audioSource.isPlaying) {
             audioSource.Stop();
         }
+    }    
+    
+    public static void FadeOut(this AudioSource audioSource, float duration) {
+        CoroutineUtil.Start(audioSource.fadeOut(duration, () => audioSource.StopIfPlaying()));
     }
 
     public static IEnumerator fadeOut(this AudioSource audioSource, float duration, Action onComplete) {
@@ -31,6 +35,29 @@ public static class AudioSourceExtensions {
         if (onComplete != null)
             onComplete();
     }
-	
+
+    public static void FadeIn(this AudioSource audioSource, float volumeTo, float duration) {
+        CoroutineUtil.Start(audioSource.fadeIn(volumeTo, duration, null));
+    }
+
+    public static IEnumerator fadeIn(this AudioSource audioSource, float volumeTo, float duration, Action onComplete) {
+
+        audioSource.volume = 0f;
+
+        audioSource.Play();
+
+        //float startingVolume = audioSource.volume;
+        float endingVolume = volumeTo;
+        
+        // fade out the volume
+        while (audioSource.volume < volumeTo) {
+            audioSource.volume += Time.deltaTime * endingVolume / duration;
+            yield return null;
+        }
+        
+        // all done fading out
+        if (onComplete != null)
+            onComplete();
+    }
 	
 }
