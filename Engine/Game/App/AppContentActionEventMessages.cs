7e997c7561aff5f7a96d9ea95921402d2cf35aba
drawcode <ryan@drawcode.com>
2016-12-21 03:03:06 -0700

    Remove events messages and move to lib-base

---
+++ Engine/Game/App/AppContentActionEventMessages.cs
@@ -0,0 +1,31 @@
+﻿
+// --------------------------------------------------------------------------------
+
+public class AppContentActionEventMessages {
+    public static string AppActionTimelineEvent = "AppActionTimelineEvent";
+    //
+    public static string AppActionEvent = "AppActionEvent";
+    public static string AppActionExternalLink = "AppActionExternalLink";
+    public static string AppActionAudioEvent = "AppActionAudioEvent";
+    public static string AppActionVideoEvent = "AppActionVideoEvent";
+    public static string AppActionInternalLinkEvent = "AppActionInternalLinkEvent"; // TODO
+    public static string AppActionPointEvent = "AppActionPointEvent";
+    public static string AppActionAssetEvent = "AppActionAssetEvent";
+    public static string AppActionTipEvent = "AppActionTipEvent";
+
+    // Play custom content
+    public static string AppActionPlayContentObjectEvent = "AppActionPlayContentObjectEvent";
+    public static string AppActionStopContentObjectEvent = "AppActionStopContentObjectEvent";
+    public static string AppActionResetContentObjectEvent = "AppActionResetContentObjectEvent";
+    public static string AppActionPauseContentObjectEvent = "AppActionPauseContentObjectEvent";
+    public static string AppActionNextContentObjectEvent = "AppActionNextContentObjectEvent";
+    public static string AppActionPreviousContentObjectEvent = "AppActionPreviousContentObjectEvent";
+    public static string AppActionRestartContentObjectEvent = "AppActionRestartContentObjectEvent";
+    public static string AppActionNextContentAppEvent = "AppActionNextContentAppEvent";
+    public static string AppActionPreviousContentAppEvent = "AppActionPreviousContentAppEvent";
+
+    // Collectables
+
+    public static string AppActionPointCollectEvent = "AppActionPointCollectEvent";
+
+}
